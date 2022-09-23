52be2bca012ab6f549fdddb8ebf598246b0b3b59
dreamLogicc <gondin02@mail.ru>
2022-09-23 21:35:21 +0700

    Task 2: added visualization

--- BlockMatrixMultiplication.cpp
+++ BlockMatrixMultiplication.cpp
@@ -1,50 +1,38 @@
 #include <iostream>
 #include <time.h>
+#include <math.h>
 
 
-int **createMatrix(int size)
-{
+int **createMatrix(int size){
     int **matrix = new int *[size];
     for (int i = 0; i < size; i++)
-    {
         matrix[i] = new int[size];
-    }
+
     for (int i = 0; i < size; i++)
-    {
         for (int j = 0; j < size; j++)
-        {
             matrix[i][j] = 1 + rand() % 100;
-        }
-    }
     return matrix;
 }
 
 // Function fills matrix with zeros
-void initZero(int **matr, int size)
-{
+void initZero(int **matr, int size){
     for (int i = 0; i < size; i++)
-    {
         for (int j = 0; j < size; j++)
-        {
             matr[i][j] = 0;
-        }
-    }
 }
 
-void mult(int **A, int **B, int **C, int n, int blockCount)
-{
+void mult(int **A, int **B, int **C, int n, int blockCount){
     int sblock = n / blockCount;
-    for (int ib = 0; ib < blockCount; ib++)
-        for (int jb = 0; jb < blockCount; jb++)
-            for (int kb = 0; kb < blockCount; kb++)
-                for (int i = 0; i < sblock; i++)
-                    for (int j = 0; j < sblock; j++)
-                        for (int k = 0; k < sblock; k++)
+    for (int kb = 0; kb < blockCount; kb++)
+        for (int ib = 0; ib < blockCount; ib++)
+            for (int jb = 0; jb < blockCount; jb++)
+                for (int k = 0; k < sblock; k++)
+                    for (int i = 0; i < sblock; i++)
+                        for (int j = 0; j < sblock; j++)
                             C[ib * sblock + i][jb * sblock + j] += A[ib * sblock + i][kb * sblock + k] * B[kb * sblock + k][jb * sblock + j];
 }
 
-unsigned int timeOf(void (*multiply)(int **, int **, int **, int, int), int **A, int **B, int **C, int size, int block)
-{
+unsigned int timeOf(void (*multiply)(int **, int **, int **, int, int), int **A, int **B, int **C, int size, int block){
     unsigned int start = clock();
     multiply(A, B, C, size, block);
     unsigned int end = clock();
@@ -52,18 +40,15 @@ unsigned int timeOf(void (*multiply)(int **, int **, int **, int, int), int **A,
     return time;
 }
 
-void clear(int **matrix, int size)
-{
+void clear(int **matrix, int size){
     for (int i = 0; i < size; i++)
-    {
         delete[] matrix[i];
-    }
     delete[] matrix;
 }
 
-int main()
-{
+int main(){
     srand(time(0));
+    
     int sizes[4] = {512, 1024, 1536, 2048};
     int blockCount[10];
 
@@ -78,10 +63,8 @@ int main()
         for (int j = 1; j <= 10; j++)
         {
             initZero(C, sizes[i]);
-            if ((sizes[i] - blockCount[j] > 0) && (sizes[i] % blockCount[j] == 0))
-            {
+            if ((sizes[i] - blockCount[j] >= 0) && (sizes[i] % blockCount[j] == 0))
                 printf("Time for %dx%d and for block count %d is %f \n", sizes[i], sizes[i], blockCount[j], (double)timeOf(mult, A, B, C, sizes[i], blockCount[j]) / 1000);
-            }
         }
         clear(A, sizes[i]);
         clear(B, sizes[i]);
